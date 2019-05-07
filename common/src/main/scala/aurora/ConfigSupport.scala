5adcb4ce90a73a47d2d4f4ed77d85ecf96010c6d
sean.hawkins <seanphawkins@gmail.com>
2019-05-07 15:34:52 -0400

    JWT support

--- common/src/main/scala/aurora/ConfigSupport.scala
+++ common/src/main/scala/aurora/ConfigSupport.scala
@@ -3,9 +3,11 @@ package aurora
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.model.headers.{Authorization, RawHeader}
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
 import com.typesafe.config.{Config, ConfigFactory}
+
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
@@ -15,12 +17,12 @@ trait ConfigSupport { this: App =>
     implicit val mat = ActorMaterializer()
     implicit val ec = system.dispatcher
     implicit val timeout = Timeout(10.second)
+    val token = ""
     val configKey = util.Properties.envOrNone("CONFIG.KEY")
-    val configEnv = util.Properties.envOrElse("CONFIG.ENV", "dev")
     val configServer = util.Properties.envOrElse("CONFIG.SERVER", "localhost:9933")
     val rcs: String = configKey.map { ck =>
       Await.result(
-        Http().singleRequest(HttpRequest(uri = s"http://$configServer/$configEnv/$ck")).flatMap(_.entity.toStrict(10.seconds).mapTo[String]),
+        Http().singleRequest(HttpRequest(uri = s"http://$configServer/$ck").addHeader(RawHeader("Authorization", token))).flatMap(_.entity.toStrict(10.seconds).mapTo[String]),
         10.seconds
       )
     }.getOrElse("{}")
