39043822ea698802a944bab358db1597aec2dd51
sean.hawkins <seanphawkins@gmail.com>
2019-05-08 13:19:53 -0400

    JWT support

--- common/src/main/scala/aurora/ConfigSupport.scala
+++ common/src/main/scala/aurora/ConfigSupport.scala
@@ -3,29 +3,39 @@ package aurora
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.HttpRequest
-import akka.http.scaladsl.model.headers.{Authorization, RawHeader}
+import akka.http.scaladsl.model.headers.RawHeader
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
 import com.typesafe.config.{Config, ConfigFactory}
-
+import akka.http.scaladsl.unmarshalling.Unmarshal
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.io.Source
 
-trait ConfigSupport { this: App =>
+trait MetaConfig {
+  def configServer: String
+  def configKey: String
+  def token: String
+}
+
+trait EnvMetaConfig extends MetaConfig {
+  val configKey = util.Properties.envOrElse("CONFIG.KEY", "DEFAULT")
+  val configServer = util.Properties.envOrElse("CONFIG.SERVER", "localhost:9933")
+  val tokenFile = util.Properties.envOrElse("CONFIG.KEYFILE", "./.configaccesskey")
+  val token = Source.fromFile(tokenFile).getLines().mkString
+}
+
+trait ConfigSupport { this: App with MetaConfig =>
   implicit val config: Config = {
     implicit val system = ActorSystem("configTempSystem", ConfigFactory.empty)
     implicit val mat = ActorMaterializer()
     implicit val ec = system.dispatcher
     implicit val timeout = Timeout(10.second)
-    val token = ""
-    val configKey = util.Properties.envOrNone("CONFIG.KEY")
-    val configServer = util.Properties.envOrElse("CONFIG.SERVER", "localhost:9933")
-    val rcs: String = configKey.map { ck =>
-      Await.result(
-        Http().singleRequest(HttpRequest(uri = s"http://$configServer/$ck").addHeader(RawHeader("Authorization", token))).flatMap(_.entity.toStrict(10.seconds).mapTo[String]),
-        10.seconds
-      )
-    }.getOrElse("{}")
+    val rcs: String = Await.result(
+      Http().singleRequest(HttpRequest(uri = s"http://$configServer/$configKey").addHeader(RawHeader("Authorization", token)))
+        .flatMap(resp => Unmarshal(resp.entity).to[String]),
+      10.seconds
+    )
     ConfigFactory.parseString(rcs).withFallback(ConfigFactory.load()).resolve
   }
 }
---
+++ common/src/test/scala/aurora/ConfigSupportTest.scala
@@ -0,0 +1,38 @@
+package aurora
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
+import org.scalatest.{FlatSpec, Matchers}
+
+class ConfigSupportTest extends FlatSpec with Matchers {
+  implicit val system = ActorSystem("ConfigSupportTest", ConfigFactory.empty)
+  implicit val mat  = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
+  val route =
+    path("TEST") {
+      get {
+        complete("""test.string = "foo"""")
+      }
+    }
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 9876)
+
+  "ConfigSupport" should "" in {
+    val testApp = new TestApp
+    testApp.testConfigString shouldBe "foo"
+    testApp.testConfigString2 shouldBe "bar"
+  }
+}
+
+class  TestApp extends App with ConfigSupport with TestMetaConfig {
+  def testConfigString = config.getString("test.string")
+  def testConfigString2 = config.getString("test.string2")
+}
+
+trait TestMetaConfig extends MetaConfig {
+  override def configServer = "localhost:9876"
+  override def configKey = "TEST"
+  override def token = "xxxxxxxxxxxx"
+}
